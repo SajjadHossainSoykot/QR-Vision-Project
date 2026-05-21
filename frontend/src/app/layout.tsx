cdb9bb024d5e8eb7d3fd1438b501d69d21e64f61
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-21 19:28:02 +0600

    update layout.tsx with latest tailwind

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "QR Vision",
@@ -15,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
