90daefcf5d5f42dcee51478ea2afeb36fa95427b
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-21 19:15:58 +0600

    update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 
 export const metadata: Metadata = {
-  title: "QR Vision SS",
-  description: "Fullstack QR Code Encode Decode Platform",
+  title: "QR Vision",
+  description:
+    "QR code generation and detection system using FastAPI, OpenCV, and Next.js.",
 };
 
 export default function RootLayout({
@@ -23,11 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
